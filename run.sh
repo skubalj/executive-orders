3507ef93068f413a5910c7b6fb8070f7d4fd361d
Joseph Skubal <skubalj@protonmail.com>
2025-05-10 15:05:19 -0500

    add party affiliation and first 100 days

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "Updating test data"
 go run . ./executive_orders.csv
 
 echo "Knitting rmarkdown"
-R --vanilla --quiet -e "rmarkdown::render('executive-orders.Rmd')" < /dev/null
+Rscript --vanilla -e "rmarkdown::render('executive-orders.Rmd')"
