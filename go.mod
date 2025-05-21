4dcc663c1d11f7fb18e4273df3c2b651c612c384
Joseph Skubal <skubalj@protonmail.com>
2025-05-21 01:11:03 -0500

    re-add go update data script

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/skubalj/executive-orders
+
+go 1.24.3
+
+require (
+	github.com/alexflint/go-arg v1.5.1
+	golang.org/x/sync v0.14.0
+)
+
+require github.com/alexflint/go-scalar v1.2.0 // indirect
--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "Updating test data"
-Rscript ./update-data.R ./executive_orders.csv
+
+EXECUTIVE_ORDER_FILE="executive_orders.csv"
+
+# Rscript ./update-data.R "$EXECUTIVE_ORDER_FILE"
+go run ./update-data.go $EXECUTIVE_ORDER_FILE
+date --iso-8601 > last_update
 
 echo "Knitting rmarkdown"
 Rscript --vanilla -e "rmarkdown::render('executive-orders.Rmd')"
