0d114434d57ee9df87730a0a999ef65a20d667f0
Joseph Skubal <skubalj@protonmail.com>
2025-06-03 23:50:19 -0500

    improve update-data script

--- run.sh
+++ run.sh
@@ -3,15 +3,7 @@
 EXECUTIVE_ORDER_FILE="executive_orders.csv"
 
 echo "Updating test data"
-
-deno run \
-    --allow-read="$EXECUTIVE_ORDER_FILE" \
-    --allow-write="$EXECUTIVE_ORDER_FILE" \
-    --allow-net="www.federalregister.gov:443" \
-    ./update-data.ts \
-    "$EXECUTIVE_ORDER_FILE"
-
-# Rscript ./update-data.R "$EXECUTIVE_ORDER_FILE"
+Rscript ./update-data.R "$EXECUTIVE_ORDER_FILE"
 date --iso-8601 > last_update
 
 echo "Knitting rmarkdown"
--- update-data.R
+++ update-data.R
@@ -1,57 +1,57 @@
 library(argparser)
 
-args <- arg_parser("Pull executive orders from the federal register") |>
+args <- arg_parser(
+  "Pull executive orders from the federal register",
+  hide.opts = TRUE
+) |>
   add_argument("output_file", help = "The output CSV file") |>
   parse_args()
 
 library(httr2)
-library(lubridate, warn.conflicts = FALSE)
-library(stringr)
+library(glue)
+library(purrr)
 library(dplyr, warn.conflicts = FALSE)
 library(readr)
-library(glue)
 
-page_size <- 1000
-documents_url <- function(page_number) {
-  stringr::str_c(
-    "https://www.federalregister.gov/api/v1/documents.csv",
-    glue("?per_page={page_size}"),
-    glue("&page={page_number}"),
-    "&fields[]=title",
-    "&fields[]=citation",
-    "&fields[]=document_number",
-    "&fields[]=president",
-    "&fields[]=signing_date",
-    "&fields[]=publication_date",
-    "&order=newest",
-    "&conditions[type][]=PRESDOCU",
-    "&include_pre_1994_docs=true",
-    "&conditions[presidential_document_type][]=executive_order"
-  )
-}
-
-fetch_page <- function(page_number) {
-  df <- request(documents_url(current_page)) |>
-    req_perform() |>
-    resp_body_string() |>
-    I() |>
-    readr::read_csv(show_col_types = FALSE)
-  print(glue("Fetched page {page_number} ({nrow(df)} records)"))
-  df
-}
-
-current_page <- 1
-page_df <- fetch_page(current_page)
-df <- page_df
-while (nrow(page_df) == page_size) {
-  current_page <- current_page + 1
-  page_df <- fetch_page(current_page)
-  df <- bind_rows(df, page_df)
-}
-
-df <- df |> mutate(
-  publication_date = mdy(publication_date),
-  president = str_split_fixed(president, "; ", 4)[, 4]
+url <- stringr::str_c( # nolint
+  "https://www.federalregister.gov/api/v1/documents.json",
+  "?per_page=1000",
+  "&fields[]=title",
+  "&fields[]=citation",
+  "&fields[]=document_number",
+  "&fields[]=president",
+  "&fields[]=signing_date",
+  "&fields[]=publication_date",
+  "&order=newest",
+  "&conditions[type][]=PRESDOCU",
+  "&include_pre_1994_docs=true",
+  "&conditions[presidential_document_type][]=executive_order"
 )
 
-write_csv(df, args$output_file)
+page_number <- 1
+while (!is.null(url)) {
+  res <- request(url) |>
+    req_perform() |>
+    resp_body_json()
+
+  url <- res[["next_page_url"]]
+
+  res[["results"]] |>
+    list_transpose(
+      template = c(
+        "title",
+        "citation",
+        "document_number",
+        "president",
+        "signing_date",
+        "publication_date"
+      ),
+      default = NA,
+    ) |>
+    as_tibble() |>
+    mutate(president = map_chr(president, ~ .x[["name"]])) |>
+    write_csv(args[["output_file"]], na = "", append = page_number != 1)
+
+  print(glue("Fetched page {page_number} ({length(res[['results']])} records)"))
+  page_number <- page_number + 1
+}
