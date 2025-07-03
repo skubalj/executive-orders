e787b376af1c996785c7b221e9ea53e514e0158f
Joseph Skubal <skubalj@protonmail.com>
2025-07-03 15:59:40 -0500

    update date to work on mac

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ EXECUTIVE_ORDER_FILE="executive_orders.csv"
 
 echo "Updating test data"
 Rscript ./update-data.R "$EXECUTIVE_ORDER_FILE"
-date --iso-8601 > last_update
+date -Idate > last_update
 
 echo "Knitting rmarkdown"
 Rscript --vanilla -e "rmarkdown::render('executive-orders.Rmd')"
